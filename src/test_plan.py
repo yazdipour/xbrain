46d5adc5dc9767f9e56e521d14f2be620cdf7511
Shahriar <yazdipour@users.noreply.github.com>
2023-06-29 02:55:03 +0200

    improve test a bit

--- src/test_plan.py
+++ src/test_plan.py
@@ -9,15 +9,13 @@ class Test_Pipe(unittest.TestCase):
     sample_text = "This is a sample text"
 
     def test_add_youtube_url(self):
-        # test add_youtube_url method
-        subject, url, html = pipe.create_content(
-            f"/add {self.sample_youtube}")
-        # assert html contains <html>
-        assert "<html>" in html
+        _, _, html = pipe.create_content(f"/add {self.sample_youtube}")
+        assert "URL:" in html
+    
+    def test_add_webpage_url(self):
+        subject, _, html = pipe.create_content(f"/add {self.sample_webpage}")
+        assert "" in html # raw url should not have html
+        assert subject == self.sample_webpage
 
-
-# def test_get_url(self):
-#     # test get_url method
-#     self.assertEqual(pipe.get_url("https://www.google.com"), "webpage")
 if __name__ == "__main__":
     unittest.main()
